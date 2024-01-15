7c9dce3c41e4402022951fb96590ab750778d172
changhui98 <net_0102@naver.com>
2024-01-15 09:04:42 +0900

    게시글 등록 기능 구현

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,8 +38,13 @@ public class ArticleController {
         return "";
     }
     @GetMapping("/articles/{id}")  // 데이터 조회 요청 접수
-    public String show(@PathVariable Long id){ // 매개변수로 id 받아 오기
+    public String show(@PathVariable Long id, Model model){ // 매개변수로 id 받아 오기
         log.info("id = " + id);
-        return "";
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
