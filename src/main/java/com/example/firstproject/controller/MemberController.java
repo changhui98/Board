387dd747a21bcc9f4ce324693bdf1f07dd7742de
changhui98 <net_0102@naver.com>
2024-01-17 00:23:42 +0900

    회원 마이페이지 수정하기 기능 구현

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -54,4 +54,35 @@ public class MemberController {
         return "members/index";
     }
 
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        // 수정할 데이터 가져오기
+        Member member = memberRepository.findById(id).orElse(null);
+
+        // 모델에 데이터 등록하기
+        model.addAttribute("member", member);
+
+        // 뷰 페이지 설정하기
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form){
+        // 1. DTO를 엔티티로 변환하기
+        Member member = form.toEntity();
+        log.info(member.toString());
+
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(member.getId()).orElse(null);
+
+        // 2-2. 기존 데이터 값을 갱신하기
+        if(target != null){
+            memberRepository.save(member);
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/members/" + member.getId();
+    }
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
+INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
+INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
+INSERT INTO member(id, email, password) VALUES(1, 'user01@test.org', 'test');
+INSERT INTO member(id, email, password) VALUES(2, 'user02@test.org', 'test');
+INSERT INTO member(id, email, password) VALUES(3, 'user03@test.org', 'test');
