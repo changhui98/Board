8b23df47ae76760ffe9f0b79d280b42da7ea04d2
changhui98 <net_0102@naver.com>
2024-01-19 01:24:42 +0900

    REST API 기능 구현

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -2,10 +2,14 @@ package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
+@Data
 public class ArticleForm {
 
     private Long id;
