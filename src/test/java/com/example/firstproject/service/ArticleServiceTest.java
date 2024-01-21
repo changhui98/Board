39941110eff0c00f0f84a928183c0fe316d86282
changhui98 <net_0102@naver.com>
2024-01-21 17:57:56 +0900

    댓글 기능 구현

--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -1,9 +1,11 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,5 +51,51 @@ class ArticleServiceTest {
     @Test
     void show_실패_존재하지_않는_id(){
 
+        // 1. 예상 데이터
+        Long id = -1L; // 예상 데이터 (null) 저장
+        Article expected = null;
+
+        // 2. 실제 데이터
+        Article article = articleService.show(id); // 실제 데이터 작성
+
+        // 3. 비교 및 검증
+        assertEquals(expected, article); // 비교
+
     }
+
+    @Test
+    @Transactional
+    void create_성공_title과_content만_있는_dto_입력() {
+        // 1. 예상 데이터
+        String title = "라라라라"; // title과 content 값 임의 배정
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content); // dto 생성
+        Article expected = new Article(4L, title, content); // 예상 데이터 ㅈ저장
+
+        // 2. 실제 데이터
+        Article article = articleService.create(dto); // 실제 데이터 저장
+
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), article.toString()); // 비교
+
+    }
+
+    @Test
+    @Transactional
+    void create_실패_id가_포함된_dto_입력(){
+        // 1. 예상 데이터
+        Long id = 4L;
+        String title = "라라라라";
+        String content = "4444"; // id, title, content 값 임의 배정
+        ArticleForm dto = new ArticleForm(id, title, content); // dto 생성
+        Article expected = null; // 예상 데이터 저장
+
+        // 2. 실제 데이터
+        Article article = articleService.create(dto); // 실제 생성 결과 저장
+
+        // 3. 비교 및 검증
+        assertEquals(expected, article); // 비교
+    }
+
+
 }
