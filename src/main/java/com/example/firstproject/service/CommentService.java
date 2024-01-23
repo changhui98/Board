5a1f2f7e28302d02dce22c17ebac27ab7b9251cc
changhui98 <net_0102@naver.com>
2024-01-23 20:19:00 +0900

    댓글수정 REST 컨트롤러, 서비스 기능 구현

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -54,8 +54,16 @@ public class CommentService {
     @Transactional
     public CommentDto update(Long id, CommentDto dto){
         // 1. 댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id) // 수정할 댓글 가져오기
+                .orElseThrow(()->new IllegalArgumentException("댓글 수정 실패!" +
+                        " 대상 댓글이 없습니다.")); // 없으면 에러 메시지 출력
         // 2. 댓글 수정
+        target.patch(dto);
+
         // 3. DB로 갱신
+        Comment updated = commentRepository.save(target);
+
         // 4. 댓글 엔티티를 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(updated);
     }
 }
